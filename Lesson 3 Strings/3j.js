941c9d99352a0c2a2e8c4003542251d0657e7ee2
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-09-13 21:31:15 +0530

    Update 3j.js

--- Lesson 3 Strings/3j.js
+++ Lesson 3 Strings/3j.js
@@ -1,1 +1,4 @@
 alert(`Total cost: $${(599+295)/100}\nThank you, come again!`);
+
+alert(`Total cost: $${(599+295)/100}
+Thank you, come again!`);   // Shift+Enter for typing in next line in the Console
