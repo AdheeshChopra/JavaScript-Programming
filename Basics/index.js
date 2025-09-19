cf21742ca7ca5c9de5fc24e7f735b38de6a66a1b
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-09-19 18:23:06 +0530

    Add files via upload

---
+++ Basics/index.js
@@ -0,0 +1,19 @@
+console.log('Hello World');
+
+// let name='Adheesh';
+// console.log(name);
+
+// VARIABLES
+
+let firstName='Adheesh';
+let lastName='Chopra';
+
+let interestRate=0.3;
+interestRate=1;
+console.log(interestRate);
+
+// PRIMITIVE TYPES
+
+let name='Adheesh'; //STRING
+let age=20; //INTEGER
+let isApproved=true; //BOOLEAN - TRUE or FALSE
