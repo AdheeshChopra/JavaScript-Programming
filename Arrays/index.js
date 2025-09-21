ef22f9e6a3304b78ab392c2bc4eacbe177d27af4
Adheesh Chopra <122475898+AdheeshChopra@users.noreply.github.com>
2025-09-21 15:49:38 +0530

    Update index.js

--- Arrays/index.js
+++ Arrays/index.js
@@ -2,4 +2,16 @@ let selectedColors=['red','blue'];
 selectedColors[2]='green';
 console.log(selectedColors);
 console.log(selectedColors[0]);
+
 console.log(selectedColors.length);
+
+const numbers=[1,-1,2,3];
+
+const items=numbers
+  .filter(n=>n>=0)
+  .map(n=>({value: n}))
+  .filter(obj=>obj.value>1)
+  .map(obj=>obj.value);
+
+console.log(items);
+
